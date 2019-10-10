c7bfad67b016f41ce2d54a1323136b7d9e5ae902
storagerepo <deemsysinchr@gmail.com>
2019-10-10 18:47:13 +0530

    commit after adding required field validation for zip and state in address form.

--- custom-module/controllers/controllers.py
+++ custom-module/controllers/controllers.py
@@ -1,5 +1,6 @@
 
 from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo.addons.portal.controllers.portal import CustomerPortal
 from odoo import http
 
 class SaleController(WebsiteSale):
@@ -7,3 +8,13 @@ class SaleController(WebsiteSale):
     def checkout(self, **post):
          #the user is logged in to checkout
          return super(SaleController, self).checkout(**post)
+    
+class CustomDeemPortal(CustomerPortal):
+    @http.route(auth='user')
+    def account(self, redirect=None, **post):
+        
+        CustomerPortal.MANDATORY_BILLING_FIELDS = ["name", "phone", "email", "street", "city", "country_id","zipcode", "state_id"]
+        CustomerPortal.OPTIONAL_BILLING_FIELDS = ["vat", "company_name"]
+    
+        return CustomerPortal.account(self,redirect=None,**post)
+        
