fc0a611216d4076126787bf2e00e0b441e3209d4
storagerepo <deemsysinchr@gmail.com>
2019-11-09 13:23:18 +0530

    commit after extended limit of product in pos session.

--- pos_ticket/static/src/js/main.js
+++ pos_ticket/static/src/js/main.js
@@ -1,24 +1,34 @@
 odoo.define('pos_custom_model', function (require) {
 
-var module = require('point_of_sale.models');
-var PosBaseWidget = require('point_of_sale.BaseWidget');
-var models = module.PosModel.prototype.models;
-for(var i=0; i<models.length; i++){
-    var model=models[i];
-    if(model.model === 'res.company'){
-         model.fields.push('street');
-         model.fields.push('city');
-         model.fields.push('state_id');
-         model.fields.push('country_id');
-    } 
-}
- //To remove $ in subtotal   
-    PosBaseWidgetCustom = PosBaseWidget.include({
-    format_currency_custom: function(amount,precision){
-
-        amount = this.format_currency(amount,precision);
-        amount=amount.replace(' ','');
-        return amount;
+    var module = require('point_of_sale.models');
+    var PosBaseWidget = require('point_of_sale.BaseWidget');
+    var posDb = require('point_of_sale.DB');
+    var models = module.PosModel.prototype.models;
+    for (var i = 0; i < models.length; i++) {
+        var model = models[i];
+        if (model.model === 'res.company') {
+            model.fields.push('street');
+            model.fields.push('city');
+            model.fields.push('state_id');
+            model.fields.push('country_id');
+        }
     }
-})
+    //To remove $ in subtotal   
+    PosBaseWidgetCustom = PosBaseWidget.include({
+        format_currency_custom: function (amount, precision) {
+
+            amount = this.format_currency(amount, precision);
+            amount = amount.replace(' ', '');
+            return amount;
+        }
+    })
+
+    //To change product limit in pos session
+    posDbCustom = posDb.include({
+        init: function () {
+            this.limit = 250;
+            this._super();
+        }
+
+    });
 });
