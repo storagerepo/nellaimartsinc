66d78d41c7042e824ae60671c53e6b3166c51f44
storagerepo <deemsysinchr@gmail.com>
2019-11-11 19:08:51 +0530

    commit after overriding the add to cart and login functionality.

--- custom-module/controllers/controllers.py
+++ custom-module/controllers/controllers.py
@@ -1,16 +1,54 @@
 
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo.addons.portal.controllers.portal import CustomerPortal
+from odoo.addons.website.controllers.main import Website
 from odoo.addons.website_sale_wishlist.controllers.main import WebsiteSaleWishlist
 from odoo import http
 from odoo.http import request
 import json
+
+class loginController(Website):
+    @http.route(website=True, auth="public")
+    def web_login(self, redirect=None, *args, **kw):
+        response = super(Website, self).web_login(redirect=redirect, *args, **kw)
+        if not redirect and request.params['login_success']:
+            if request.env['res.users'].browse(request.uid).has_group('base.group_user'):
+                redirect = b'/web?' + request.httprequest.query_string
+            else:
+                redirect = '/'
+            return http.redirect_with_hash(redirect)
+        return response
 
 class SaleController(WebsiteSale):
     @http.route(auth='user')
     def checkout(self, **post):
          #the user is logged in to checkout
          return super(SaleController, self).checkout(**post)
+
+    @http.route(['/shop/cart/update'], type='http', auth="public", methods=['POST'], website=True, csrf=False)
+    def cart_update(self, product_id, add_qty=1, set_qty=0, **kw):
+        """This route is called when adding a product to cart (no options)."""
+        sale_order = request.website.sale_get_order(force_create=True)
+        if sale_order.state != 'draft':
+            request.session['sale_order_id'] = None
+            sale_order = request.website.sale_get_order(force_create=True)
+
+        product_custom_attribute_values = None
+        if kw.get('product_custom_attribute_values'):
+            product_custom_attribute_values = json.loads(kw.get('product_custom_attribute_values'))
+
+        no_variant_attribute_values = None
+        if kw.get('no_variant_attribute_values'):
+            no_variant_attribute_values = json.loads(kw.get('no_variant_attribute_values'))
+
+        sale_order._cart_update(
+            product_id=int(product_id),
+            add_qty=add_qty,
+            set_qty=set_qty,
+            product_custom_attribute_values=product_custom_attribute_values,
+            no_variant_attribute_values=no_variant_attribute_values
+        )
+        return request.redirect(request.httprequest.referrer)
     
 class CustomDeemPortal(CustomerPortal):
     @http.route(auth='user')
